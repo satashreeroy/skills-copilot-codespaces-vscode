b57aab623fd63a77a9aadb0f78866b6b023c6d68
satashreeroy <113882078+satashreeroy@users.noreply.github.com>
2024-04-07 08:01:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    var skills = ["HTML", "CSS", "JavaScript", "PHP", "MySQL"];
+    var member = {
+        name: "John",
+        age: 30,
+        skills: skills
+    };
+    return member;
+}
